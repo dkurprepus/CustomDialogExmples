c8e03a489b6483e501b8cc9aa28364a63d6c0eaa
Darshan Khatri <mailtomisterdarshan@gmail.com>
2019-04-09 01:02:03 +0530

    Initial commit

---
+++ app/src/main/java/com/exmple/androiddialog/MainActivity.java
@@ -0,0 +1,37 @@
+package com.exmple.androiddialog;
+
+import android.app.Dialog;
+import android.content.Context;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ShowDialog(this);
+    }
+
+    private void ShowDialog(Context mainActivity) {
+        final Dialog dialog=new Dialog(mainActivity);
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        dialog.setContentView(R.layout.dialog_layout);
+
+        Button btnDismiss=(Button)dialog.findViewById(R.id.btnDismiss);
+        btnDismiss.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog.dismiss();
+            }
+        });
+        dialog.show();
+    }
+}
